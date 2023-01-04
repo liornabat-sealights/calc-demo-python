dad94805528e8619621345cccce421aac39f77eb
Lior Nabat <lior.nabat@gmail.clom>
2023-01-04 08:01:39 +0200

    init

---
+++ main.py
@@ -0,0 +1,63 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/add', methods=['GET'])
+def add():
+    a = request.args.get('a')
+    b = request.args.get('b')
+
+    try:
+        a = int(a)
+        b = int(b)
+    except ValueError:
+        return "Error: Please provide valid integer values for the 'a' and 'b' parameters."
+
+    return str(int(a) + int(b))
+
+
+@app.route('/sub', methods=['GET'])
+def sub():
+    a = request.args.get('a')
+    b = request.args.get('b')
+
+    try:
+        a = int(a)
+        b = int(b)
+    except ValueError:
+        return "Error: Please provide valid integer values for the 'a' and 'b' parameters."
+
+    return str(int(a) - int(b))
+
+
+@app.route('/mul', methods=['GET'])
+def mul():
+    a = request.args.get('a')
+    b = request.args.get('b')
+
+    try:
+        a = int(a)
+        b = int(b)
+    except ValueError:
+        return "Error: Please provide valid integer values for the 'a' and 'b' parameters."
+
+    return str(int(a) * int(b))
+
+
+@app.route('/div', methods=['GET'])
+def div():
+    a = request.args.get('a')
+    b = request.args.get('b')
+
+    try:
+        a = int(a)
+        b = int(b)
+    except ValueError:
+        return "Error: Please provide valid integer values for the 'a' and 'b' parameters."
+
+    return str(int(a) / int(b))
+
+
+if __name__ == '__main__':
+    app.run()
